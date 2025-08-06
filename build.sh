0672587d5dd1f36c8010894c1a9b99b2cf16d38d
YurasovAleksey <alex14ur@yandex.ru>
2025-08-06 03:16:42 +0300

    fixed settings

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
+# здесь добавьте все необходимые команды для установки вашего проекта
+# команду установки зависимостей, сборки статики, применения миграций и другие
+make install && make collectstatic && make migrate
