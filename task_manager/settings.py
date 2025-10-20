002541bd058a0b1eb5ae1d4ec69dcf2ffef7815b
YurasovAleksey <alex14ur@yandex.ru>
2025-10-20 23:34:06 +0300

    fixed settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "tycqp$o&f&&x3up2oc($=vb=40(*kc(@mn@+m5*2i#5q)lc3+@")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = os.getenv("DEBUG", False).lower() == 'true'
 
 ALLOWED_HOSTS = [
     "localhost",
