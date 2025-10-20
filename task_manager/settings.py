87c1da220f08ae89e2d979a9dfd9a06e6921e69d
YurasovAleksey <alex14ur@yandex.ru>
2025-10-20 23:37:19 +0300

    fixed settings (dedug)

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "tycqp$o&f&&x3up2oc($=vb=40(*kc(@mn@+m5*2i#5q)lc3+@")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False).lower() == 'true'
+DEBUG = os.getenv("DEBUG", "False").lower() == 'true'
 
 ALLOWED_HOSTS = [
     "localhost",
